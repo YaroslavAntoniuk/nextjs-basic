d27d961389dad626b698a05ae7da76493364ef6f
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-07 17:35:44 +0200

    Added todo card

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,7 +1,11 @@
 import { TodoEntity } from "@/types";
 
 const Todo = ({ todo }: { todo: TodoEntity }) => {
-  return <div>{todo.content}</div>
+  return <div className="border m-2 p-4 w-64">
+    <h2>Title: {todo.title}</h2>
+    <h4>Content: {todo.title}</h4>
+    <div>Done: {todo.done}</div>
+  </div>
 }
 
 export default Todo;
