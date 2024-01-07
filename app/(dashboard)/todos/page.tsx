fde57ad27b00169aa7e5182e718c0b80bd06b51c
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-07 16:30:21 +0200

    Added todos fetch & Added todos to the page

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,7 +1,19 @@
-const Todos = () => {
+import TodoList from '@/components/TodoList';
+import db from '@/utils/db';
+
+const getTodos = async () => {
+  const todos = await db.todo.findMany({});
+
+  return todos;
+}
+
+const Todos = async () => {
+  const todos = await getTodos();
+
   return (
     <div>
       <h1>Todos Page</h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
