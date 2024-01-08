d222a64d294019bbc8e5d548fb557aa8d6014e0b
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-08 19:44:22 +0200

    Added toggle complete action

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,11 +3,20 @@ import { revalidatePath } from 'next/cache'
 import db from './db'
 
 export const createTodo = async (formData: FormData) => {
-  const todo = await db.todo.create({
+  await db.todo.create({
     data: {
       content: formData.get('content') as string,
       title: formData.get('title') as string,
     },
+  })
+
+  revalidatePath('/todos')
+}
+
+export const toggleCompleteTodo = async (id: string, done: boolean) => {
+  await db.todo.update({
+    where: { id },
+    data: { done: !done },
   })
 
   revalidatePath('/todos')
