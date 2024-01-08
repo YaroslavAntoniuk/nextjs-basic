bdaca075bc8f4bd0b1a3537783a81cc6fac49194
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-08 19:44:55 +0200

    Added toggle complete handler & styles

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,7 +1,17 @@
+'use client';
 import { TodoEntity } from "@/types";
+import { toggleCompleteTodo } from "@/utils/actions";
+import { useTransition } from "react";
 
 const Todo = ({ todo }: { todo: TodoEntity }) => {
-  return <div className="border m-2 p-4 w-64">
+  const [isPending, startTransition] = useTransition();
+
+  const handleComplete = () => {
+    startTransition(() => toggleCompleteTodo(todo.id, todo.done));
+  }
+
+  return <div className={`border m-2 p-4 w-64 relative ${todo.done ? 'bg-green-50 border-green-400' : ''}`}>
+    <div onClick={handleComplete} className="absolute top-2 right-2 text-neutral-600 text-xs cursor-pointer">{todo.done ? 'Incomplete' : 'Complete'}</div>
     <h2>Title: {todo.title}</h2>
     <h4>Content: {todo.title}</h4>
     <div>Done: {todo.done ? '✅' : '❌'}</div>
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -3,7 +3,7 @@ import Todo from "./Todo"
 
 const TodoList = ({ todos }: { todos: TodoEntity[] }) => {
   return (
-    <div className="grid grid-rows-3 grid-flow-col gap-1">
+    <div className="grid grid-rows-3 grid-flow-col gap-1 relative">
       {todos.map((todo) => (
         <Todo key={todo.id} todo={todo} />
       ))}
